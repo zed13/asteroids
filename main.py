b0cb8defff270c1b19572c4d00adb212ee76d167
Thor Odinson <thor@192.168.1.63>
2025-08-09 11:47:43 +0200

    Game constansts added

--- main.py
+++ main.py
@@ -1,7 +1,14 @@
 import pygame
+from constants import *
 
 def main():
     print("Starting Asteroids!")
+    print(f"Screen width: {SCREEN_WIDTH}")
+    print(f"Screen height: {SCREEN_HEIGHT}")
+    print(f"Asteroid min radius: {ASTEROID_MIN_RADIUS}")
+    print(f"Asteroid kinds: {ASTEROID_KINDS}")
+    print(f"Asteroid spawn rate: {ASTEROID_SPAWN_RATE}")
+    print(f"Asteroid max radius: {ASTEROID_MAX_RADIUS}")
 
 
 if __name__ == "__main__":
