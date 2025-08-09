524a9bc3a90d83009af25470087f2e8fe046fc4f
Denis Danilchuk <ddanilchuk13@gmail.com>
2025-08-09 14:24:40 +0200

    Rotation logic for ship

--- main.py
+++ main.py
@@ -20,6 +20,7 @@ def main():
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
+        player.update(dt)
         player.draw(screen)
         pygame.display.flip()
         dt = clock.tick(60) / 1_000
--- player.py
+++ player.py
@@ -24,3 +24,15 @@ class Player(CircleShape):
            points=self.triangle(),
            width=2
        )
+
+    def rotate(self, dt):
+        self.rotation += const.PLAYER_TURN_SPEED * dt
+
+    def update(self, dt):
+        keys = pygame.key.get_pressed()
+
+        if keys[pygame.K_a]:
+            self.rotate(dt)
+                # ?
+        if keys[pygame.K_d]:
+            self.rotate(-dt)
