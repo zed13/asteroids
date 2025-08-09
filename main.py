a48534dcdc066ad2eebbcadb34d89a6bc1ac935f
Denis Danilchuk <ddanilchuk13@gmail.com>
2025-08-09 15:02:32 +0200

    Asteroid field generation

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
 import pygame
 import constants as const
 from player import Player
+from asteroid import Asteroid
+from asteroidfield import AsteroidField
 
 def main():
     print("Starting Asteroids!")
@@ -14,17 +16,26 @@ def main():
     screen = pygame.display.set_mode((const.SCREEN_WIDTH, const.SCREEN_HEIGHT))
     clock = pygame.time.Clock()
     dt = 0
+
     updatable = pygame.sprite.Group()
     drawable = pygame.sprite.Group()
+    asteroids = pygame.sprite.Group()
+
     Player.containers = (updatable, drawable)
+    Asteroid.containers = (asteroids, updatable, drawable)
+    AsteroidField.containers = (updatable)
+
     player = Player(x=const.SCREEN_WIDTH/2, y=const.SCREEN_HEIGHT/2)
+    asteroid_field = AsteroidField()
+
     while True:
         screen.fill("black")
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
         updatable.update(dt)
-        drawable.draw(screen)
+        for entity in drawable:
+            entity.draw(screen)
         pygame.display.flip()
         dt = clock.tick(60) / 1_000
 
